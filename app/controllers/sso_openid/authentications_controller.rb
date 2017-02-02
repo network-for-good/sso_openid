4c9bd246e3aa406fcae2006e1ae2781b0d658cd2
Timothy King <tmk@lordzork.com>
2017-02-02 16:15:10 -0500

    Refactors controller module declaration

--- app/controllers/sso_openid/authentications_controller.rb
+++ app/controllers/sso_openid/authentications_controller.rb
@@ -1,31 +1,33 @@
-class SsoOpenid::AuthenticationsController < ApplicationController
-  def create
-    omniauth_data = env['omniauth.auth']
-    admin = Admin.from_omniauth(omniauth_data, request.subdomain)
-    if admin
-      sign_in(admin)
-      redirect_to(admin_dashboard_path) and return
-    else
-      redirect_to(admin_access_not_allowed_path) and return
+module SsoOpenid
+  class AuthenticationsController < ApplicationController
+    def create
+      omniauth_data = env['omniauth.auth']
+      admin = Admin.from_omniauth(omniauth_data, request.subdomain)
+      if admin
+        sign_in(admin)
+        redirect_to(admin_dashboard_path) and return
+      else
+        redirect_to(admin_access_not_allowed_path) and return
+      end
     end
-  end
 
-  def failure
-    raise 'failure'
-  end
+    def failure
+      raise 'failure'
+    end
 
-  def setup
-    request.env['omniauth.strategy'].options[:client_options][:redirect_uri] = callback_url('nfg-openid', subdomain: request.subdomain)
-    render :text => "Omniauth setup phase.", :status => 200
-  end
+    def setup
+      request.env['omniauth.strategy'].options[:client_options][:redirect_uri] = callback_url('sso-openid', subdomain: request.subdomain)
+      render :text => "Omniauth setup phase.", :status => 200
+    end
 
-  private
+    private
 
-  def current_donor
-    nil
-  end
+    def current_donor
+      nil
+    end
 
-  def state
-    @state ||= SecureRandom.hex(32)
+    def state
+      @state ||= SecureRandom.hex(32)
+    end
   end
 end
