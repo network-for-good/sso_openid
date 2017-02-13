a0a664eb9d81c24c323a8134eff6c215d52bf5e7
Timothy King <tmk@lordzork.com>
2017-02-13 16:16:12 -0500

    Use namespaced routes so they can be mounted manually instead of appended

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+SsoOpenid::Engine.routes.draw do
   get SsoOpenid::Paths.callback_path => "sso_openid/sessions#create", as: :sso_openid_callback
   get SsoOpenid::Paths.setup_path => "sso_openid/sessions#setup", as: :sso_openid_setup
   get SsoOpenid::Paths.auth_path => "sso_openid/sessions#new", as: :sso_openid_auth
