a4a33b430139e23fb47983f1baa1cb9cf087e608
Timothy King <tmk@lordzork.com>
2017-03-13 12:35:18 -0400

    Adds failure path

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ SsoOpenid::Engine.routes.draw do
   get SsoOpenid::Paths.callback_path => "sessions#create", as: :callback
   get SsoOpenid::Paths.setup_path => "sessions#setup", as: :setup
   get SsoOpenid::Paths.auth_path => "sessions#new", as: :auth
+  get SsoOpenid::Paths.failure_path => "sessions#failure", as: :failure
   get '/admin/sign_out' => 'sessions#destroy', as: :destroy_admin_session
 end
