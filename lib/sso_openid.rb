f7f5b56178a6446c975807014e09fd8694221abc
Dhruv Sharma <c-dhruv.sharma@bonterratech.com>
2025-10-16 12:06:37 +0530

    Temporary hardcoded Auth0 credentials for UAT until api-keys.yml is updated

--- lib/sso_openid.rb
+++ lib/sso_openid.rb
@@ -13,20 +13,31 @@ module SsoOpenid
       # Read configuration from APP_CONFIG (api-keys.yml)
       sso_config = APP_CONFIG[:sso_openid]
       
+      # Temporary hardcoded Auth0 credentials for UAT/beta environment
+      # TODO: Remove this once api-keys.yml is updated by DevOps team
+      client_id = sso_config[:client_id]
+      client_secret = sso_config[:client_secret]
+      
+      # Fallback to hardcoded Auth0 credentials if old Identity Server credentials detected
+      if client_id == 'N4GW3BAuc' || client_id == '1234' || Rails.env.beta?
+        client_id = 'MEWrh8cWedRRMFsyJQZ6HOEfuuiH8ikB'
+        client_secret = 'ZzdKZ7uctbp1ABbDHk3d8D1zTgmB-03YWVIhcQUwFtQgFOYV9u-8UXpt7odHvtXZ'
+      end
+      
       {
         client_options: {
           port: 443,
           scheme: "https",
           host: SsoOpenid::Urls.sso_openid.host,
-          identifier: sso_config[:client_id],
-          secret: sso_config[:client_secret],
+          identifier: client_id,
+          secret: client_secret,
         },
         callback_path: SsoOpenid::Paths.callback_path,
         request_path: SsoOpenid::Paths.auth_path,
         setup_path: SsoOpenid::Paths.setup_path,
         name: :sso_openid,
         discovery: true,
-        issuer: SsoOpenid::Urls.sso_openid.fqdn + "/",
+        issuer: SsoOpenid::Urls.sso_openid.discovery_endpoint,
         setup: true,
         scope: [:openid, :email, :profile, :address],
       }
