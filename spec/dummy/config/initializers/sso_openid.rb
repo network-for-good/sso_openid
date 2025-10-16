761d82702e44df7bc4e48f815fc9e155ca92b00a
Pavan Kumar K C <c-pavan.kumar@bonterratech.com>
2025-10-16 14:21:01 +0530

    Update CircleCI to use cimg Docker images

--- spec/dummy/config/initializers/sso_openid.rb
+++ spec/dummy/config/initializers/sso_openid.rb
@@ -1,6 +1,9 @@
 # Configure SsoOpenid
+api_config = YAML.load_file(Rails.root.join('config', 'api-keys.yml'))
+config_hash = api_config['defaults'].merge(api_config[Rails.env] || {})
+
 SsoOpenid.configure do |config|
-  config.identifier = APP_CONFIG[:sso_openid][:client_id]
-  config.secret = APP_CONFIG[:sso_openid][:client_secret]
+  config.identifier = config_hash['sso_openid']['client_id']
+  config.secret = config_hash['sso_openid']['client_secret']
 end
 
--- spec/lib/sso_openid/middleware_spec.rb
+++ spec/lib/sso_openid/middleware_spec.rb
@@ -1,6 +1,27 @@
 require 'rails_helper'
 
 describe SsoOpenid::Middleware, type: :request do
+  # Stub the OpenID Connect discovery endpoint
+  before(:each) do
+    issuer = SsoOpenid::Urls.sso_openid.discovery_endpoint
+    discovery_url = "#{issuer}/.well-known/openid-configuration"
+    stub_request(:get, discovery_url)
+      .to_return(
+        status: 200,
+        body: {
+          issuer: issuer,
+          authorization_endpoint: "#{issuer}/protocol/openid-connect/auth",
+          token_endpoint: "#{issuer}/protocol/openid-connect/token",
+          userinfo_endpoint: "#{issuer}/protocol/openid-connect/userinfo",
+          jwks_uri: "#{issuer}/protocol/openid-connect/certs",
+          end_session_endpoint: "#{issuer}/protocol/openid-connect/logout",
+          response_types_supported: ["code", "id_token", "id_token token", "code id_token", "code token", "code id_token token"],
+          subject_types_supported: ["public"],
+          id_token_signing_alg_values_supported: ["RS256"]
+        }.to_json,
+        headers: { 'Content-Type' => 'application/json' }
+      )
+  end
   describe "a regular request" do
     let(:response_text) { "root path" }
     before { get root_path }
@@ -11,13 +32,13 @@ describe SsoOpenid::Middleware, type: :request do
   end
 
   describe "a request to the auth path" do
+    before { get sso_openid.auth_path }
+
     let(:discovery_endpoint) { SsoOpenid::Urls.sso_openid.discovery_endpoint }
     let(:redirect_uri) { sso_openid.callback_url(subdomain: request.subdomain) }
     let(:encoded_redirect_uri) { ERB::Util.url_encode(redirect_uri) }
     let(:scope) { SsoOpenid::Configuration.openid_options[:scope] }
-    let(:client_id) { SsoOpenid::Configuration.openid_options[:client_options][:client_id] }
-
-    before { get sso_openid.auth_path }
+    let(:client_id) { SsoOpenid.configuration&.identifier }
 
     subject { response.location }
 
